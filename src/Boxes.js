6b8b8d95c4d00e9f3cb6b8e3ede4e1d3d682418f
Mira Pohjola <mira.pohjola89@gmail.com>
2018-04-23 20:20:35 +0300

    Added catching passed props for Boxes.js. Added needed styling for new elements to css. Moved algorithm under client side.

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
-const xAxis = ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10"];
-const boxes = xAxis.map((box) =>
-<div key={box.toString()} className={"box " + box.toString()}>
-    
-</div>
-);
-
-
 class Boxes extends Component {
     render() {
+        var xAxis = this.props.xAxis;
+        const boxes = xAxis.map((box) =>
+            <div key={box.toString()} className={"box " + box.toString()}>
+
+            </div>
+        );
         return (
             <div>
                 {boxes}
